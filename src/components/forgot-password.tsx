3d5561c2737d859a515443222985fc2cce789100
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2025-03-07 05:39:55 +0000

    login and registration page created

---
+++ src/components/forgot-password.tsx
@@ -0,0 +1,55 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function ForgotPasswordForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Forgot your password?</CardTitle>
+          <CardDescription>
+            Enter your email below and we&apos;ll send you a reset link
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Send Reset Link
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Remember your password?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Login
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
